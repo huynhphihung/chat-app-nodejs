1eb34d8fdb01e4c96366998eda4de5e4cc81f3fc
huynhphihung <phihunghuynh2005@gmail.com>
2024-03-28 05:18:08 +0700

    fixing socket url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -5,35 +5,31 @@ import { io } from "socket.io-client";
 export const SocketContext = createContext();
 
 export const useSocketContext = () => {
-  return useContext(SocketContext);
+	return useContext(SocketContext);
 };
 
 export const SocketContextProvider = ({ children }) => {
-  const [socket, setSocket] = useState(null);
-  const [onlineUser, setOnlineUser] = useState([]);
-  const { authUser } = useAuthContext();
-  useEffect(() => {
-    if (authUser) {
-      const socket = io("http://localhost:5000", {
-        query: {
-          userId: authUser._id,
-        },
-      });
-      setSocket(socket);
-      socket.on("getOnlineUsers", (users) => {
-        setOnlineUser(users);
-      });
-      return () => socket.close();
-    } else {
-      if (socket) {
-        socket.close();
-        setSocket(null);
-      }
-    }
-  }, [authUser]);
-  return (
-    <SocketContext.Provider value={{ socket, onlineUser }}>
-      {children}
-    </SocketContext.Provider>
-  );
+	const [socket, setSocket] = useState(null);
+	const [onlineUser, setOnlineUser] = useState([]);
+	const { authUser } = useAuthContext();
+	useEffect(() => {
+		if (authUser) {
+			const socket = io("https://chat-app-product-yzio.onrender.com", {
+				query: {
+					userId: authUser._id,
+				},
+			});
+			setSocket(socket);
+			socket.on("getOnlineUsers", (users) => {
+				setOnlineUser(users);
+			});
+			return () => socket.close();
+		} else {
+			if (socket) {
+				socket.close();
+				setSocket(null);
+			}
+		}
+	}, [authUser]);
+	return <SocketContext.Provider value={{ socket, onlineUser }}>{children}</SocketContext.Provider>;
 };
